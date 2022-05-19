e57f55ab8385dd73545484f9b7332dc6cc66f061
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-05-18 22:16:37 -0500

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Fundamentos-Angular-My-Store';
+  name = 'Cesar';
+  age = 27;
+  img = 'https://1.bp.blogspot.com/-xm6qY-hkYOc/X4MbD0L7C1I/AAAAAAABDuo/znkKx2jkf5kRoIte-rQpNb4hfryTUdJTACLcBGAsYHQ/s2048/EjS_hIfUcAA1nGh.jpg';
 }
