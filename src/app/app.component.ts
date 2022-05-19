206a1f78d165d4161fc68c0dadfb4941065f9f41
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-05-19 16:39:23 -0500

    Property Blinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   name = 'Cesar';
   age = 27;
   img = 'https://1.bp.blogspot.com/-xm6qY-hkYOc/X4MbD0L7C1I/AAAAAAABDuo/znkKx2jkf5kRoIte-rQpNb4hfryTUdJTACLcBGAsYHQ/s2048/EjS_hIfUcAA1nGh.jpg';
+
+  btnDisabled = false;
+
+  person = {
+    name: 'Cesar',
+    age: 27,
+    img: 'https://1.bp.blogspot.com/-xm6qY-hkYOc/X4MbD0L7C1I/AAAAAAABDuo/znkKx2jkf5kRoIte-rQpNb4hfryTUdJTACLcBGAsYHQ/s2048/EjS_hIfUcAA1nGh.jpg'
+  }
 }
