545cc8457c813f2d0fde233abc4b474b0ef3f5be
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-05-19 17:09:22 -0500

    Introducción al Event Binding de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,12 @@ export class AppComponent {
     age: 27,
     img: 'https://1.bp.blogspot.com/-xm6qY-hkYOc/X4MbD0L7C1I/AAAAAAABDuo/znkKx2jkf5kRoIte-rQpNb4hfryTUdJTACLcBGAsYHQ/s2048/EjS_hIfUcAA1nGh.jpg'
   }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
 }
